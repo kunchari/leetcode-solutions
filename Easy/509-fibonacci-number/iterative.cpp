517ecfedff4911e2cdc2afd84ca58a8181b7a956
Arina Kunchenko <107347228+kunchari@users.noreply.github.com>
2026-02-12 23:46:02 +0100

    Create iterative.cpp

---
+++ Easy/509-fibonacci-number/iterative.cpp
@@ -0,0 +1,21 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+
+        int prev2 = 0;
+        int prev1 = 1;
+        int current = 0;
+      
+        for (int i = 2; i <= n; i++) {
+            current = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+};
